8be285ca01e8523c1bba3a99627e27ed366f9059
Priyanshu-dev-source <priyanshu2004ss@gmail.com>
2025-03-06 00:48:18 +0530

    PWA added

--- serviceWorkerRegistration.js
+++ serviceWorkerRegistration.js
@@ -7,7 +7,7 @@
 // existing tabs open on the page have been closed, since previously cached
 // resources are updated in the background.
 
-// To learn more about the benefits of this model and instructions on how to
+// To learn more about the benefits of this model and instructions on how to open
 // opt-in, read https://cra.link/PWA
 
 const isLocalhost = Boolean(
